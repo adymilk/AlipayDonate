78f36ff4072f6852c11476401935ae31774037ad
adymilk <924114103@qq.com>
2017-08-19 11:51:16 +0800

    修复逻辑代码

--- app/src/main/java/com/adymilk/alipaydonate/MainActivity.java
+++ app/src/main/java/com/adymilk/alipaydonate/MainActivity.java
@@ -1,5 +1,7 @@
 package com.adymilk.alipaydonate;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         Button button1 = (Button) findViewById(R.id.button1);
         Button button2 = (Button) findViewById(R.id.button2);
         Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
 
 
 
@@ -44,10 +47,24 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     String Url = editText.getText().toString();
+
+//                    用户输入不为空的情况下判断
                     if (!(Url.isEmpty())) {
-                       String pay_key = Url.substring(22,Url.length());
-                        System.out.println(pay_key);
-                        AlipayZeroSdk.startAlipayClient(MainActivity.this, pay_key);
+//                        用户输入不合法的情况判断
+                        if (Url.length()>44){
+                            String substring = Url.substring(0,22);
+                            if (substring.equals("HTTPS://QR.ALIPAY.COM/")){
+                                String pay_key = Url.substring(22,Url.length());
+//                            System.out.println(pay_key);
+                                AlipayZeroSdk.startAlipayClient(MainActivity.this, pay_key);
+                            }else {
+                                Toast.makeText(MainActivity.this, "url 不合法！", Toast.LENGTH_SHORT).show();
+//                            System.out.println(substring);
+                            }
+                        }else{
+                            Toast.makeText(MainActivity.this, "url 不合法！", Toast.LENGTH_SHORT).show();
+                        }
+//                       用户输入为空的情况下提示
                     }else {
                         Toast.makeText(MainActivity.this, "不能为空！", Toast.LENGTH_SHORT).show();
                     }
@@ -76,10 +93,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                joinQQGroup("JXqo5mGe-tD5-KJEhvumvUuMJnweqmA4");
+            }
+        });
 
     }
+
+    /****************
+     *
+     * 发起添加群流程。群号：超级脚本软件交流群(539300032) 的 key 为： JXqo5mGe-tD5-KJEhvumvUuMJnweqmA4
+     * 调用 joinQQGroup(JXqo5mGe-tD5-KJEhvumvUuMJnweqmA4) 即可发起手Q客户端申请加群 超级脚本软件交流群(539300032)
+     *
+     * @param key 由官网生成的key
+     * @return 返回true表示呼起手Q成功，返回fals表示呼起失败
+     ******************/
+    public boolean joinQQGroup(String key) {
+        Intent intent = new Intent();
+        intent.setData(Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26k%3D" + key));
+        // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        try {
+            startActivity(intent);
+            return true;
+        } catch (Exception e) {
+            // 未安装手Q或安装的版本不支持
+            return false;
+        }
+    }
+
 }
 
